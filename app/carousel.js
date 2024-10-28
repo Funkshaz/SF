c7cc09eddb80f93a3d4408f424d7a4da5749a271
Kylan Vike <kylan@mtnview.ca>
2024-10-28 14:29:01 -0700

    Initial commit

---
+++ app/carousel.js
@@ -0,0 +1,8 @@
+export default function Carousel({ heading }) {
+  return (
+    <>
+      <h1>{heading}</h1>
+      test
+    </>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Carousel from "./carousel";
+
+export default function Home() {
+  return (
+    <>
+      <Carousel heading="Test Heading" />
+    </>
+  );
+}
